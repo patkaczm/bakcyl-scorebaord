af3a6e1fd0b7d35b08bd078e35c6763b72a48a1a
Kaczmarek Patryk <patryk.kaczmarek@nokia.com>
2019-11-26 12:54:04 +0100

    add registration for advanced group based on hash

--- register/views.py
+++ register/views.py
@@ -3,8 +3,11 @@ from .forms import RegisterForm
 from bakcyl_scoreboard import urls as bakcyl_urls
 from django.contrib.auth import authenticate, login
 from .models import PersonalInfo
+from bakcyl_scoreboard.models import Group
 
-def register(response):
+ADVANCED_GROUP_HASH = 'c7d3369addf613226291dbfd574f82f8'
+
+def register(response, group_hash = 0):
     if response.user.is_authenticated:
         return redirect("dashboard")
     if response.method == "POST":
@@ -18,6 +21,8 @@ def register(response):
             personal_info.codewars_name = form.cleaned_data['codewars_username']
             personal_info.first_name = form.cleaned_data['first_Name']
             personal_info.last_name = form.cleaned_data['last_Name']
+            if (group_hash == ADVANCED_GROUP_HASH):
+                personal_info.group_level = Group.Advanced
             personal_info.save()
 
             new_user = authenticate(username=form.cleaned_data['username'],
