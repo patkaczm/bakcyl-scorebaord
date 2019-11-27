50ca094da5e1b2f0d9b2160c520ed01b84f01b44
Kaczmarek Patryk <patryk.kaczmarek@nokia.com>
2019-11-27 15:09:22 +0100

    add user and tutor view for tasks and users

--- bakcyl_scoreboard/models.py
+++ bakcyl_scoreboard/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-
 class Group():
     Beginner = '0'
     Advanced = '1'
--- bakcyl_scoreboard/views.py
+++ bakcyl_scoreboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Task, Solution, Score
 from django.contrib.auth.models import User
 from .forms import SolutionForm, CommentForm, TaskForm
@@ -32,11 +32,36 @@ def get_tasks(user):
                 tasks['obligatory']['done'].append(t)
             except:
                 tasks['obligatory']['notdone'].append(t)
+    tasks['mandatory']['donecount']=len(tasks['mandatory']["done"])
+    tasks['mandatory']['notdonecount']=len(tasks['mandatory']["notdone"])
+    tasks['obligatory']['donecount']=len(tasks['obligatory']["done"])
+    tasks['obligatory']['notdonecount']=len(tasks['obligatory']["notdone"])
     return tasks
 
 def index(request):
     tasks = Task.objects.all()
-    return render(request, "bakcyl_scoreboard/dashboard.html", {"tasks":tasks})
+    all = get_tasks(request.user)
+    return render(request, "bakcyl_scoreboard/dashboard.html", {"tasks":tasks,
+                                                                "all":all})
+
+def dashboard_tutor(request):
+    students = PersonalInfo.objects.filter(isTutor=False)
+    selectedStudent = None
+    selectedTask = None
+
+    if request.method == "POST":
+        if "showStatisticsUser" in request.POST:
+            selectedStudent = request.POST.get("showStatisticsUser")
+        if "showStatisticsTask" in request.POST:
+            selectedTask = request.POST.get("showStatisticsTask")
+            selectedTask = Task.objects.get(id=selectedTask)
+
+    return render(request, "bakcyl_scoreboard/dashboard_tutor.html", {
+                                                                        "tasks":Task.objects.all(),
+                                                                        "students":students,
+                                                                        "selectedStudent": selectedStudent,
+                                                                        "selectedTask":selectedTask,
+                                                                    })
 
 def task_detail_tutor(response, task_id):
     solutuion = None
@@ -122,6 +147,9 @@ def task_detail(response, task_id):
         return task_detail_tutor(response, task_id)
 
 def add_task(response):
+    if not PersonalInfo.objects.get(user=response.user).isTutor:
+        return redirect("dashboard")
+
     if (response.method == 'POST'):
         form = TaskForm(response.POST)
         if (form.is_valid()):
@@ -137,3 +165,52 @@ def add_task(response):
     return render(response, "bakcyl_scoreboard/add_task.html", {
                                                                 "tasks":Task.objects.all(),
                                                                 "form":form})
+
+def user_chart_data(response, user):
+    tasks = get_tasks(User.objects.get(username=user))
+    m_done = len(tasks['mandatory']['done'])
+    m_todo = len(tasks['mandatory']['notdone'])
+    o_done = len(tasks['obligatory']['done'])
+    o_todo = len(tasks['obligatory']['notdone'])
+
+    return JsonResponse({
+        'mandatory':{
+            'done':m_done,
+            'todo':m_todo,
+        },
+        'obligatory':{
+            'done':o_done,
+            'todo':o_todo,
+        },
+    })
+
+def task_chart_data(response, task_id):
+    task = Task.objects.get(id=task_id)
+
+    done = Solution.objects.filter(task=task, isFinal = True).count()
+    students = PersonalInfo.objects.filter(isTutor=False)
+    students_count = students.count()
+    todo = students_count - done
+    
+    score = {
+        'users':[],
+        'score':[],
+    }
+
+    for student in students:
+        points = 0
+        try:
+            points = Solution.objects.get(user = student.user, task = task)
+            points = points.score.mark
+        except: pass
+        finally:
+            pi = PersonalInfo.objects.get(user=student.user)
+            score['users'].append(pi.first_name +" "+pi.last_name)
+            score['score'].append(points)
+
+    return JsonResponse({
+        'name': task.name,
+        'done': done,
+        'todo': todo,
+        'score': score,
+    })
