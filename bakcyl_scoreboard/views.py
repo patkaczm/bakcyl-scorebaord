3d9a350f96b73b0cfe5e80f33cc7c3b953cc9dd1
patryk <patryk.kaczmarek996@gmail.com>
2020-03-24 20:01:14 +0100

    completed new dashboard

--- bakcyl_scoreboard/views.py
+++ bakcyl_scoreboard/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render, redirect
 from register.models import PersonalInfo
+import requests
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+def getUserTasksData(user):
+    ret_data = {}
+
+    codewars_nick = PersonalInfo.objects.get(user=user).codewars_name
+    tasks = requests.get('https://www.codewars.com/api/v1/users/{}/code-challenges/completed'.format(codewars_nick)).json()['data']
+
+    for task in tasks:
+        if "cpp" in task["completedLanguages"]:
+            task_rank = -requests.get('https://www.codewars.com/api/v1/code-challenges/{}'.format(task['id'])).json()["rank"]["id"]
+
+            if not task_rank in ret_data:
+                ret_data[task_rank] = []
+
+            ret_data[task_rank].append({"name": task["name"],
+                                        "time": task["completedAt"]})
+
+    ret = []
+    for kyu in sorted(ret_data.keys()):
+        ret.append({
+            "kyu": kyu,
+            "tasks": ret_data[kyu]
+        })
+    return ret
+
+def calculatePoints(tasks):
+    points = 0
+    for task in tasks:
+        points += len(task["tasks"]) * (100 - (10 * task["kyu"]))
+    return points
 
 
-def dashboard(response):
-    if not response.user.is_authenticated:
+def dashboard(request):
+    if not request.user.is_authenticated:
         return redirect("login")
 
-    return render(response, "bakcyl_scoreboard/dashboard.html")
+    data = getUserTasksData(request.user)
+    points = calculatePoints(data)
+
+    return render(request, "bakcyl_scoreboard/dashboard.html", {
+        "task_data": data,
+        "points": points
+    })
 
 
 def dashboard_tutor(request):
